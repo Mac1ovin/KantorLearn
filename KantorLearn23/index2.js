c6c2306af9a14d394e1d8b0bb05520077efc2b22
Mac1ovin <Vasyalena777@gmail.com>
2018-08-08 13:06:37 +0300

    reload 23 folder

--- KantorLearn23/index2.js
+++ KantorLearn23/index2.js
@@ -1,7 +1,8 @@
 ("use strict");
+//____________________________________Вариант 1 Цикл
 function fib(n) {
-  var x = 1;
-  var y = 1;
+  var a = 1;
+  var b = 1;
 
   for (var i = 3; i <= n; i++) {
     var c = a + b;
@@ -13,3 +14,14 @@ function fib(n) {
 alert(fib(3)); // 2
 alert(fib(7)); // 13
 alert(fib(77)); // 5527939700884757
+// ____________________________________Вариант 2 Рекурсия
+// function fib(n) {
+//   if (n > 1) {
+//     return fib(n - 1) + fib(n - 2);
+//   } else {
+//     return n;
+//   }
+//   fib(n);
+// }
+// alert(fib(7)); // 13
+// alert(fib(3)); // 2
