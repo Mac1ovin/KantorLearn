4d85f276ea988b48689e69f04dfdae68e06e3d79
Mac1ovin <Vasyalena777@gmail.com>
2018-08-27 16:10:26 +0300

    add folder38 task 1

--- KantorLearn32/index.js
+++ KantorLearn32/index.js
@@ -1,30 +1,30 @@
 "use strict";
 //_____________________________________________Task 1__________________________\\
-function ucFirst(str) {
-  return str.charAt(0).toUpperCase() + str.slice(1);
-}
-console.log(ucFirst("xXX"));
+// function ucFirst(str) {
+//   return str.charAt(0).toUpperCase() + str.slice(1);
+// }
+// console.log(ucFirst("xXX"));
 //_____________________________________________Task 2__________________________\\
-function checkSpam(str) {
-  var result = str.toLowerCase();
-  if (~result.indexOf("viagra") || ~result.indexOf("xxx"))
-    return console.log(true);
-  checkSpam("buy ViAgRA now") == true;
-  checkSpam("free xxxxx") == true;
-  checkSpam("innocent rabbit") == false;
-}
+// function checkSpam(str) {
+//   var result = str.toLowerCase();
+//   if (~result.indexOf("viagra") || ~result.indexOf("xxx"))
+//     return console.log(true);
+//   checkSpam("buy ViAgRA now") == true;
+//   checkSpam("free xxxxx") == true;
+//   checkSpam("innocent rabbit") == false;
+// }
 //_____________________________________________Task 3__________________________\\
-function truncate(str, maxlength) {
+function Clip(str, maxlength) {
   if (str.length > maxlength) {
-    var truncateStr = str.substring(0, maxlength - 3).trim();
-    return truncateStr + "...";
+    var ClipStr = str.substring(0, maxlength + 1);
+    return ClipStr + "...";
   }
 }
+console.log(Clip("Вот, что мне хотелось бы сказать на эту тему:", 20));
 
-console.log(truncate("Вот, что мне хотелось бы сказать на эту тему:", 25));
------------------------------------------------------------
-function extractCurrencyValue(str) {
-   return +str.slice(1)
-}
+//______________________________________________Task 4___________________________\\
+// function extractCurrencyValue(str) {
+//   return +str.slice(1);
+// }
 
-console.log(extractCurrencyValue('$120'));
+// console.log(extractCurrencyValue("$120"));
---
+++ KantorLearn38/index.js
@@ -0,0 +1,7 @@
+"use strict";
+var arr = ["Есть", "жизнь", "на", "Марсе"];
+var nameLengths = arr.map(function(name) {
+  return name.length;
+});
+alert(nameLengths); // 4,5,2,5
+LarrLength;
